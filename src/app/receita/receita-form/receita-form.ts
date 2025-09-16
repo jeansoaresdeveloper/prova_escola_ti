a46107f81accee5fb44d01fe60bcee82adc0fa21
jean.soares <jean.soares@elotech.com.br>
2025-09-15 21:15:03 -0300

    feat: form simples com mock

--- src/app/receita/receita-form/receita-form.ts
+++ src/app/receita/receita-form/receita-form.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ReceitaService } from '../receita';
 
 @Component({
   selector: 'app-receita-form',
   imports: [
     ReactiveFormsModule
+  ],
+  providers: [
+    ReceitaService
   ],
   templateUrl: './receita-form.html',
   styleUrl: './receita-form.scss'
@@ -12,9 +17,14 @@ import { FormGroup, ReactiveFormsModule, UntypedFormBuilder } from '@angular/for
 export class ReceitaForm implements OnInit {
 
   form: FormGroup<any> = new FormGroup({});
+  ingredientForm: FormGroup<any> = new FormGroup({});
+
+  showIngredientes = false;
 
   constructor(
-    private readonly builder: UntypedFormBuilder
+    private readonly service: ReceitaService,
+    private readonly builder: FormBuilder,
+    private readonly router: Router
   ) {}
 
   ngOnInit() {
@@ -23,8 +33,65 @@ export class ReceitaForm implements OnInit {
       id: [''],
       nome: [''],
       tempoPreparo: [''],
+      custoAproximado: [''],
+      ingredientes: this.builder.array([
+        {
+          id: 'teste',
+          nome: 'teste',
+        },
+        {
+          id: 'teste',
+          nome: 'teste',
+        },
+        {
+          id: 'teste',
+          nome: 'teste',
+        }
+      ])
     });
 
+  }
+
+  buildFormIngredient() {
+    this.ingredientForm = this.builder.group({
+      id: [''],
+      nome: ['']
+    })
+  }
+
+  back() {
+    this.router.navigate(['']);
+  }
+
+  save() {
+
+    if (this.form.invalid) return;
+
+    this.service.create(this.form.value)
+      .subscribe();
+  }
+
+  openIngredientes() {
+    this.showIngredientes = !this.showIngredientes;
+  }
+
+  editarIngrediente() {
+
+    if (this.isEdit()) {
+
+    }
+
+  }
+
+  deletarIngrediente() {
+
+    if (this.isEdit()) {
+
+    }
+  }
+
+  private isEdit() {
+    return this.form.get('id')?.value
   }
 
 }
